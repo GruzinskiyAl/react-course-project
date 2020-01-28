0a801df20d4b466fff3e9a7f02564ce03bbb6c46
Gruzinskiy <gruzinskiy.al@gmail.com>
2020-01-28 11:43:47 +0200

    added shop provider, added product route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import "./App.css";
 import ProductProvider from "./providers/ProducrsProvider";
 import ProductsRoute from "./routes/ProductsRoute";
+import ProductRoute from "./routes/ProductRoute";
 
 export default function App() {
-  return (
-    <ProductProvider>
-      <Router>
-        {/* <AppHeader /> */}
-        <Switch>
-          <Route path="/products">
-            <ProductsRoute />
-          </Route>
-          <Route path="*">
-            <Redirect to="/products" />
-          </Route>
-        </Switch>
-      </Router>
-    </ProductProvider>
-  );
+    return (
+        <ProductProvider>
+            <Router>
+                {/* <AppHeader /> */}
+                <Switch>
+                    <Route path="/products/:productId">
+                        <ProductRoute/>
+                    </Route>
+                    <Route path="/products">
+                        <ProductsRoute/>
+                    </Route>
+                    <Route path="*">
+                        <Redirect to="/products"/>
+                    </Route>
+                </Switch>
+            </Router>
+        </ProductProvider>
+    );
 }
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Product from "../ui/Product";
+import ProductsHeader from "../ui/ProductsHeader";
 
 export default function ProductList({products}) {
   return (
     <div>
-      {products.map(({ id, name, description, date, price, origin, createdAt, updatedAt }) => (
+      <ProductsHeader />
+      {products.map(({ id, name, price, origin, createdAt, updatedAt }) => (
         <Product
           key={id}
           id={id}
           name={name}
-          description={description}
-          date={date}
           price={price}
           origin={origin}
           createdAt={createdAt}
